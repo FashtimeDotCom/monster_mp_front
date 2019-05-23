021902076649135fa87adff2a7a97479a1815565
夏佳昊 <xiajiahao@xiajiahaodeMacBook-Pro.local>
2019-05-24 07:37:26 +0800

    [feat] 处理转发逻辑

--- pages/jump/toPage10003.js
+++ pages/jump/toPage10003.js
@@ -1,5 +1,8 @@
 var Page = global.GioPage;
 Page({
+  onLoad(opt) {
+    getApp().turnToPage('/pages/page10003/page10003?detail=' + (opt.detail || '446433'), true);
+  },
   onShow() {
     getApp().turnToPage('/pages/page10003/page10003?detail=446433', true);
   }
